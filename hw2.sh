6873b43bb740e231658f6039014669291ef07303
bacon_cy <58839511+bacon-cy@users.noreply.github.com>
2023-03-23 23:18:04 +0800

    Finished Parsing Options

--- hw2.sh
+++ hw2.sh
@@ -27,13 +27,13 @@ if [ $# -le 0 ]; then
 	exit 1
 fi
 
-while getopts "hi:-:" opt; do
-	case $opt in
-		h) #help
+while [ "$#" -gt 0 ]; do
+	case "$1" in
+		-h) #help
 			usage
 			exit 0
 		;;
-		i) #input files
+		-i) #input files
 			shift
 			while [ $# -gt 0 ] && ! [[ "$1" == -* ]]; do
 				files+=("$1")
@@ -41,26 +41,8 @@ while getopts "hi:-:" opt; do
 				shift
 			done
 		;;
-		-) #hashes
-		
-			#確認hash數量
-			case ${OPTARG} in
-				md5*)
-					echo ${OPTARG}
-					md5=1
-				;;
-				sha256*)
-					echo ${OPTARG}
-					sha256=1
-				;;
-				*)
-					echo "Error: Invalid arguments." 1>&2
-					usage
-					exit 1
-				;;
-			esac
-
-			#輸入hashes
+		--md5)
+			md5=1
 			shift
 			while [ $# -gt 0 ] && ! [[ "$1" == -* ]]; do
 				echo "$1"
@@ -69,23 +51,37 @@ while getopts "hi:-:" opt; do
 				shift
 			done
 		;;
+		--sha256)
+			sha256=1
+			shift
+			while [ $# -gt 0 ] && ! [[ "$1" == -* ]]; do
+				echo "$1"
+				hashes+=("$1")
+				hashnum=$(( hashnum + 1 ))
+				shift
+			done
+		;;	
 		*) #invaid options inputed
+			if [[ "$1" != "-md5" ]] || [[ "$1" != "-sha256" ]]; then
 			echo "Error: Invalid arguments." 1>&2
 			usage
 			exit 1
+			fi
 		;;
 	esac
 done
-			
-if [ $md5 -eq 1 ] && [ $sha256 -eq 1 ]; then
-	echo "Error: Only one type of hash function is allowed." 1>&2
-	exit 3
-fi
 
 if [ $hashnum -ne $filenum ]; then
 	echo "Error: Invalid values." 1>&2
 	exit 2
 fi
+	
+if [ $md5 -eq 1 ] && [ $sha256 -eq 1 ]; then
+	echo "Error: Only one type of hash function is allowed." 1>&2
+	exit 3
+fi
+
+
 
 
 
